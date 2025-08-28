e84eb12f3e88e15e74c2b2bae224cbd4c3cfc501
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-28 22:57:10 +0530

    Added Data Ingestion

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -13,7 +13,21 @@ def read_yaml(path_to_yaml:Path)->ConfigBox: # what is ConfigBox
         with open(path_to_yaml)as yaml_file:
             content=yaml.safe_load(yaml_file)
             logging.info(f"Yaml file:{path_to_yaml} loaded successfully")
+            return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
+    
+@ensure_annotations
+def create_directories(path_to_directories:list,verbose=True):
+    """
+    create list of directories
+    Args:
+        path_to_directories(list): list of path of directories
+        ignore_log(bool, optional):ignore if multiple dirs to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logging.info(f"Created Directory at: {path}")
