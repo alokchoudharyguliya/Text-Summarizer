75ef0274e8791e83ba70113796db0841e688f571
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-29 02:18:52 +0530

    Backend Added

---
+++ app.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+import uvicorn,sys,os
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+from fastapi.responses import Response
+from src.textSummarizer.pipeline.prediction_pipeline import PredictionPipeline
+
+text:str="What is Text Summarization?"
+app=FastAPI()
+@app.get("/",tags=['authorization'])
+async def index():
+    return RedirectResponse(url="/docs")
+
+@app.get("/train")
+async def training():
+    try:
+        os.system("python main.py")
+        return Response("Training Successful")
+    except Exception as e:
+        return Response(f"Error Occurred : {e}")
+    
+@app.post("/predict")
+async def predict_route(text):
+    try:
+        obj=PredictionPipeline()
+        text=obj.predict(text=text)
+        return text
+    except Exception as e:
+        raise e
+
+
+if __name__=="__main__":
+    uvicorn.run(app,host='0.0.0.0',port=8080)
